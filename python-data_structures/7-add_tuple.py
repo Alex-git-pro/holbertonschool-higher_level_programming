1097e592d7e8b6787eec30dda480966f3ee13d22
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-01-21 21:47:01 +0700

    correct 7 add

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a1, a2 = tuple_a + (0, 0)
-    b1, b2 = tuple_b + (0, 0)
-    return (a1 + b1, a2 + b2)
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    return (a[0] + b[0], a[1] + b[1])
