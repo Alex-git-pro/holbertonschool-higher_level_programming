6ab47da8db1e21e321ffba17d54d35110e375fad
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-01-24 18:25:22 +0700

    add 4 list division

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+
+    for i in range(list_length):
+        try:
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                raise IndexError("out of range")
+
+            if (not isinstance(my_list_1[i], (int, float)) or
+               not isinstance(my_list_2[i], (int, float))):
+                raise TypeError("wrong type")
+
+            result = my_list_1[i] / my_list_2[i]
+
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+
+        except IndexError as e:
+            print(str(e))
+            result = 0
+
+        except TypeError as e:
+            print(str(e))
+            result = 0
+
+        except Exception as e:
+            print(f"Error: {e}")
+            result = 0
+
+        finally:
+            result_list.append(result)
+
+    return result_list
