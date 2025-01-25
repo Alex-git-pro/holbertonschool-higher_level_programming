0e80b107e0f6a43c1ec0ff3bcb814096ff5a41f0
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-01-25 18:02:23 +0700

    add 0 add integer and .txt

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,7 +3,6 @@
 Module to add two integers or floats.
 """
 
-
 def add_integer(a, b=98):
     """
     Adds two integers or floats.
@@ -35,7 +34,7 @@ def add_integer(a, b=98):
     TypeError: a must be an integer
     """
     if not isinstance(a, (int, float)):
-        raise TypeError("b must be an integer")
+        raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     a = int(a)
