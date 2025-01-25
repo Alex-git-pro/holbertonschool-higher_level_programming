77e84e1f8f0188fcd7a63c68703d3f426fd27fa9
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-01-25 18:49:31 +0700

    Wow

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+def print_square(size):
+    """
+    Prints a square of size 'size' using the character '#'.
+
+    Arguments:
+    size -- the size (length of the side) of the square
+
+    If `size` is not an integer, raises TypeError with the message "size must be an integer".
+    If `size` is a float and less than 0, raises TypeError with the message "size must be an integer".
+    If `size` is less than 0, raises ValueError with the message "size must be >= 0".
+    """
+    if type(size) != int:
+        if type(size) == float:
+            raise TypeError("size must be an integer")
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    
+    for i in range(size):
+        print("#" * size)
