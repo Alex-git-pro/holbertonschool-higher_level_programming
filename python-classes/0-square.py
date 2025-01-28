c5f943dd2a2ee025d1c4ccd7b6cd1fc9196d5e38
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-01-29 02:52:26 +0700

    Add 2-3-4-5-6

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,38 @@
 #!/usr/bin/python3
 """
 Defines a class Square.
+
+This module defines a class that represents a square. The class is currently
+empty but serves as a placeholder for future square-related logic, such as 
+calculating the area or the perimeter.
+
+Attributes:
+    None: This class does not have any attributes or methods yet. It is 
+    an empty class intended for future extensions.
+
+Todo:
+    * Implement methods to calculate the area and perimeter of the square.
+    * Add validation to ensure that the size attribute is a positive number.
+
+Examples:
+    >>> square = Square()
+    >>> print(type(square))
+    <class '__main__.Square'>
 """
 
 class Square:
     """
     Empty class that defines a square.
-    
-    This class has no attributes or methods yet.
-    It is a placeholder for future square-related logic.
+
+    This class currently does not contain any attributes or methods. It is 
+    intended to serve as a basic placeholder for future extensions, where 
+    methods to handle square-related operations will be added.
+
+    Attributes:
+        None: No attributes have been defined for this class yet.
+
+    Todo:
+        * Implement attributes for the side length of the square.
+        * Add methods to calculate area and perimeter.
     """
     pass
