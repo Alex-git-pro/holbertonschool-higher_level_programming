5616d6cc47010af88701e716825536c420974680
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-01-29 02:41:13 +0700

    Add 2-3-4-5-6

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+
 class Square:
+    """
+    A class used to represent a square.
+
+    The square is defined by its side length. It provides methods to
+    calculate the area and the perimeter of the square.
+
+    Attributes:
+        side_length (float): The length of the side of the square.
+    """
     pass
