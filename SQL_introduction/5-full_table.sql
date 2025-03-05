254af2a1b54dfce96140d9357b819abfa9f4cf0c
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-03-05 17:03:44 +0700

    Push

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,12 +1,2 @@
 -- Prints the description of the table first_table
-SELECT 
-    table_name AS TABLE_NAME, 
-    column_name AS COLUMN_NAME, 
-    column_type AS COLUMN_TYPE, 
-    is_nullable AS IS_NULLABLE, 
-    column_default AS COLUMN_DEFAULT
-FROM 
-    information_schema.columns
-WHERE 
-    table_name = 'first_table'
-    AND table_schema = 'hbtn_0c_0';
+SHOW CREATE TABLE first_table
